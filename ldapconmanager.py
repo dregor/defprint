2c3737f3cfd890b8d537d1efceebfb7f8348b1fb
Олещук Егор Владимирович <  >
2017-01-25 11:44:06 +0300

    add returning by uid or sid

--- ldapconmanager.py
+++ ldapconmanager.py
@@ -3,33 +3,36 @@ from getpass import getpass
 
 
 class LdapConManager():
-
-
 	def __init__(self, domain, server, user='', password=''):
 		self.domain = domain
-		# self.lc = ldap.controls.SimplePagedResultsControl(True, size=self.PAGE_SIZE, cookie='')
-		try:
-			self.connection = LdapPagedSearch('ldap://' + server, user, password, maxPages=1000, pageSize=500)
-		except Exception as e:
-			print(e)
+		self.connection = LdapPagedSearch('ldap://' + server, user, password, maxPages=1000, pageSize=500)
 
 
-	def get_user_list(self):
+	def getAllUsers(self):
 		filter=('(&(objectclass=person)(uid=*))')
 		attrs=(['uid'])
 		for user in self.connection.search(self.domain, filter, attrs):
 			yield(user)
 
 
-	def get_user_sid(self, uid=None):
-		filter_list= ['(objectclass=sambaSamAccount)','(sambaSID=*)']
-		if uid is not None:
+	def getUserSid(self, uid=None, sid=None):
+		filter_list= ['(objectclass=sambaSamAccount)']
+		if uid:
 			filter_list.append('(uid=%s)' % (uid))
+		if sid:
+			filter_list.append('(sambaSID=%s)' % (sid))
 		filter = '(&' + ''.join(filter_list) + ')'
 		attrs=(['uid','sambaSID'])
+		for dn, data in self.connection.search(self.domain, filter, attrs):
+			if sid:
+				yield {data['sambaSID'][0]: {'uid':data['uid'][0]}}
+			else:
+				yield {data['uid'][0]: {'sid':data['sambaSID'][0]}} 
+
+	def getUserSidList(self, uid=None, sid=None):
 		result = {}
-		for dn, sid in self.connection.search(self.domain, filter, attrs):
-			result.update( {sid['uid'][0]: {'sid':sid['sambaSID'][0]}} )
+		for user in self.getUserSid(uid, sid):
+			result.update(user)
 		return result
 
 
@@ -38,9 +41,10 @@ if __name__=='__main__':
 	server = '172.16.0.111'	
 	ldap_connection = LdapConManager(domain, server)
 	if not isinstance( ldap_connection, str ):
-		users = ldap_connection.get_user_sid()
-		for item in users:
-			print(item)
-			print(users[item])
+		users = ldap_connection.getUserSidList()
+		for user in users:
+			print(user)
+			for item in users[user]:
+				print(item + ': ' + users[user][item])
 	else:
 		print(ldap_connection)
